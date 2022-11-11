678fe68fd7837fe0735e6d17a04aef316f992962
JingChao <jc.jingchao@qq.com>
2022-11-11 14:33:45 +0800

    update tabbar-custom

--- tabbar-custom-0/pages/men/men.js
+++ tabbar-custom-0/pages/men/men.js
@@ -26,7 +26,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        selected: 2
+      })
+    }
   },
 
   /**
