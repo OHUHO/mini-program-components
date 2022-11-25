e4d64a2fd9be4a141a9632b1372ec57031e6aece
JingChao <jc.jingchao@qq.com>
2022-11-25 22:04:17 +0800

    update

--- custom-navbar-three/app.js
+++ custom-navbar-three/app.js
@@ -1,22 +1,34 @@
 App({
   onLaunch() {
+    this.getNavBarInfo()
+  },
 
+
+  // 获取应用的导航栏信息
+  getNavBarInfo(){
     // 获取系统信息
     const systemInfo = wx.getSystemInfoSync();
-    console.log(systemInfo)
     // 胶囊按钮位置信息
-    const menuButtonInfo = wx.getMenuButtonBoundingClientRect();
-    console.log(menuButtonInfo)
+    const menuInfo = wx.getMenuButtonBoundingClientRect();
+    
     // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上距离-状态栏高度） * 2 + 胶囊高度 + 状态栏高度
-    this.globalData.navBarHeight = (menuButtonInfo.top - systemInfo.statusBarHeight) * 2 + menuButtonInfo.height; //导航栏高度
-    this.globalData.statusBarHeight = systemInfo.statusBarHeight; //状态栏高度
-    this.globalData.menuRight = systemInfo.screenWidth - menuButtonInfo.right;  //胶囊距离右方的间距
-    this.globalData.menuBotton = menuButtonInfo.top - systemInfo.statusBarHeight; //胶囊距离底部的间距
-    this.globalData.menuHeight = menuButtonInfo.height; //胶囊高度
-    this.globalData.menuWidth = menuButtonInfo.width; //胶囊宽度
-    this.globalData.windowWidth = systemInfo.windowWidth; //屏幕宽度
+    var navBarHeight = (menuInfo.top - systemInfo.statusBarHeight) * 2 + menuInfo.height; //导航栏高度
+    var statusBarHeight = systemInfo.statusBarHeight; //状态栏高度
+    var menuRight = systemInfo.screenWidth - menuInfo.right;  //胶囊距离右方的间距
+    var menuBotton = menuInfo.top - systemInfo.statusBarHeight; //胶囊距离底部的间距
+    var menuHeight = menuInfo.height; //胶囊高度
+    var menuWidth = menuInfo.width; //胶囊宽度
+    var windowWidth = systemInfo.windowWidth; //屏幕宽度
 
+    this.globalData.navBarHeight = navBarHeight
+    this.globalData.statusBarHeight = statusBarHeight
+    this.globalData.menuRight = menuRight
+    this.globalData.menuBotton = menuBotton
+    this.globalData.menuHeight = menuHeight
+    this.globalData.menuWidth = menuWidth
+    this.globalData.windowWidth = windowWidth
   },
+
   globalData: {
     navBarHeight: 0, 
     statusBarHeight: 0, 
--- custom-navbar-three/index/index.js
+++ custom-navbar-three/index/index.js
@@ -3,7 +3,8 @@ const app = getApp()
 Page({
   data: {
     menuHeight: 0,
-    menuTop: 0,
+    menuMarginTop: 0,
+    
     isRefresh: false,
     currentTab: 0,
 
@@ -21,41 +22,23 @@ Page({
   },
   onLoad(){
     const res = wx.getMenuButtonBoundingClientRect()
+    var height = res.height //胶囊高度
+    var top = res.top //胶囊距离顶部的高度
     this.setData({
-      menuHeight: res.height,
-      menuTop: res.top
+      menuHeight: height,
+      menuMarginTop: top,
     })
   
   },
 
-  // 获取应用的导航栏信息
-  getNavBarInfo(){
-    // 获取系统信息
-    const systemInfo = wx.getSystemInfoSync();
-    // 胶囊按钮位置信息
-    const menuInfo = wx.getMenuButtonBoundingClientRect();
-    
-    // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上距离-状态栏高度） * 2 + 胶囊高度 + 状态栏高度
-    var navBarHeight = (menuInfo.top - systemInfo.statusBarHeight) * 2 + menuInfo.height; //导航栏高度
-    var statusBarHeight = systemInfo.statusBarHeight; //状态栏高度
-    var menuRight = systemInfo.screenWidth - menuInfo.right;  //胶囊距离右方的间距
-    var menuBotton = menuInfo.top - systemInfo.statusBarHeight; //胶囊距离底部的间距
-    var menuHeight = menuInfo.height; //胶囊高度
-    var menuWidth = menuInfo.width; //胶囊宽度
-    var windowWidth = systemInfo.windowWidth; //屏幕宽度
 
-    this.setData({
-
-    })
-
-  },
-
-  tabNav(e) {
+  selectBar(e) {
     let currentTab = e.currentTarget.dataset.index
     this.setData({
       currentTab
     })
   },
+
   handleSwiper(e) {
     let {current,source} = e.detail
     if (source === 'autoplay' || source === 'touch') {
