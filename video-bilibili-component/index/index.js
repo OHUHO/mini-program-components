571e09fdf70a835d276f48488a211c9e81c4e703
JingChao <jc.jingchao@qq.com>
2022-11-25 16:01:41 +0800

    create picture-waterfall-flow

--- video-bilibili-component/index/index.js
+++ video-bilibili-component/index/index.js
@@ -15,7 +15,7 @@ Page({
   inputValue: '',
   data: {
     // 视频封面
-    poster: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2Fedpic%2Fcd%2F05%2Fdc%2Fcd05dc90a63739a47d7b301a02205b7e.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1670910759&t=60761c88d0f4240feda3a49a98b1cfe7',
+    poster: '/images/7.jpeg',
     src: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
     danmuList: [
       {
@@ -24,37 +24,9 @@ Page({
         time: 1
       },
       {
-        text: '第 2s 出现的弹幕',
-        color: '#ff00ff',
-        time: 2
-      },
-      {
         text: '第 3s 出现的弹幕',
         color: '#ff00ff',
         time: 3
-      },{
-        text: '第 4s 出现的弹幕',
-        color: '#ff0000',
-        time: 4
-      },
-      {
-        text: '第 5s 出现的弹幕',
-        color: '#ff00ff',
-        time: 5
-      },
-      {
-        text: '第 5s 出现的弹幕',
-        color: '#ff00ff',
-        time: 5
-      },{
-        text: '第 5s 出现的弹幕',
-        color: '#ff0000',
-        time: 5
-      },
-      {
-        text: '第 8s 出现的弹幕',
-        color: '#ff00ff',
-        time: 8
       },
       {
         text: '第 36s 出现的弹幕',
