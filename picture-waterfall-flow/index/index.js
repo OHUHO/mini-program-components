e947dfe56d1b0fb0c8cf67c9d71d8a2a9e2bfde0
JingChao <jc.jingchao@qq.com>
2022-11-25 16:09:43 +0800

    finish component

--- picture-waterfall-flow/index/index.js
+++ picture-waterfall-flow/index/index.js
@@ -2,10 +2,56 @@ const app = getApp()
 
 Page({
   data: {
+    videoList: [
+      {
+        id: 1,
+        name: '昵称',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/2.jpg',
+        avatar: '/images/6.jpeg'
+      },
+      {
+        id: 2,
+        name: '昵称2',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/4.png',
+        avatar: '/images/8.jpeg'
+      },
+      {
+        id: 3,
+        name: '昵称3',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/7.jpeg',
+        avatar: '/images/7.jpeg'
+      },
+      {
+        id: 4,
+        name: '昵称4',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/3.jpg',
+        avatar: '/images/6.jpeg'
+      },
+      {
+        id: 5,
+        name: '昵称5',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/6.jpeg',
+        avatar: '/images/grill.jpeg'
+      },
+      {
+        id: 6,
+        name: '昵称6',
+        num: '99',
+        title: '测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测测试数据测试测试测试测',
+        url: '/images/8.jpeg',
+        avatar: '/images/8.jpeg'
+      },
+    ]
+  },
 
-  },
-  onLoad() {
-    console.log('代码片段是一种迷你、可分享的小程序或小游戏项目，可用于分享小程序和小游戏的开发经验、展示组件和 API 的使用、复现开发问题和 Bug 等。可点击以下链接查看代码片段的详细文档：')
-    console.log('https://mp.weixin.qq.com/debug/wxadoc/dev/devtools/devtools.html')
-  },
 })
